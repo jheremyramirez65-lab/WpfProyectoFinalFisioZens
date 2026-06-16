f37eda8f75a5370dbc03747227893cff6ed2626e
jheremyramirez65-lab <jheremyramirez65@gmail.com>
2026-06-16 11:04:14 -0400

    Agregar archivos de proyecto.

---
+++ WpfProyectoFinal/WinLogin.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfProyectoFinal
+{
+    /// <summary>
+    /// Lógica de interacción para WinLogin.xaml
+    /// </summary>
+    public partial class WinLogin : Window
+    {
+        public WinLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciarSesion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCrearCuenta_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfProyectoFinal/WinSignUp.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfProyectoFinal
+{
+    /// <summary>
+    /// Lógica de interacción para WinSignUp.xaml
+    /// </summary>
+    public partial class WinSignUp : Window
+    {
+        public WinSignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
